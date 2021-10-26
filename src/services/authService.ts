7895e9dc4d7f1afd173f21471ffd8caa6e3d0a03
Roman Miroshnikov <ramiroshnikov@vtb.ru>
2021-10-26 13:14:26 +0300

    refactor

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -49,7 +49,7 @@ export const authApi = createApi({
                 } finally {
                     dispatch(setGlobalLoading(false));
                 }
-            }
+            },
         })
     })
 });
